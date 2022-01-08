0f46af7be209392ded161912b14a856597dd4594
Rahul Kumar Ranjan <kjrahul21@gmail.com>
2022-01-08 19:57:09 +0530

    dsf

--- college_management/settings.py
+++ college_management/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from .env import credentials
 import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--*!5=0hlf776u!fj-0@*rlj77$#z#@pd=-4kg+bgzr#u9sh8w6'
+SECRET_KEY = credentials.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
